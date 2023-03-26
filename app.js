bcdafb81c766151f5750d8c47ae173756c1436cd
Abhishek Bansal <92670086+2509abhi@users.noreply.github.com>
2023-03-26 11:41:03 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,39 +3,24 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.listen(process.env.PORT || 3000);
-app.get("/9/9", function(req,res){
+app.get("/999955183433", function(req,res){
     res.send({
         "uid": "999955183433",
         "name": "Rohit Pandey",
         "dob": "08-07-1985",
-        "dobt": "A",
         "gender": "M",
         "phone": "2821096353",
-        "email": "rpandey@mailserver.com",
-        "building": "603/4 Vindyachal",
-        "street": "7TH Road Raja Wadi",
-        "locality": "Neelkanth Valley",
-        "poname": "Ghatkopar (EAST)",
-        "vtc": "Mumbai",
-        "district": "Mumbai",
-        "state": "Maharastra",
-        "pincode": "110072"
+        "Address": "603/4 Vindyachal, Delhi"
         });
 });
-app.get("/9/2", function(req,res){
+app.get("/929990501894", function(req,res){
     res.send({
         "uid": "929990501894",
         "name": "Anisha Jay Kapoor",
-        "gender": "F",
         "dob": "01-01-1982",
-        "dobt": "V",
-        "building": "2B 203",
-        "street": "14 Main Road",
-        "locality": "Jayanagar",
-        "district": "Bangalore",
-        "state": "Karnataka",
-        "pincode": "110011"
-        
+        "gender": "F",
+        "phone": "8821096353",
+        "Address": "2B 203"
         });
 });
 // {
